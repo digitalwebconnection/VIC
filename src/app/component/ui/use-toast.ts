b8d7e967bbcc87376b5a2b98ef1d07c7160d8cd2
digitalwebconnection <service@digitalwebconnection.com>
2025-08-25 11:30:25 +0530

    first commit4

--- src/app/component/ui/use-toast.ts
+++ src/app/component/ui/use-toast.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
 // Inspired by react-hot-toast library
@@ -6,7 +7,7 @@ import * as React from "react"
 import type {
   ToastActionElement,
   ToastProps,
-} from "@/components/ui/toast"
+} from "./toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
@@ -158,7 +159,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
-      onOpenChange: (open) => {
+      onOpenChange: (open: boolean) => {
         if (!open) dismiss()
       },
     },
